88aba32aaab1f902595b832d5b536e399cd2591b
James Curran <james.curran@gmail.com>
2023-10-20 11:24:31 -0400

    added debugging

--- frontend/src/views/Inbox.js
+++ frontend/src/views/Inbox.js
@@ -9,20 +9,21 @@ import { Link } from 'react-router-dom'
 var  SERVER_URL=process.env.SERVER_URL;
 SERVER_URL="http://localhost:8000"
 
-export default function Inbox() {
+export default function  Inbox() {
     console.log("Start of Inbox");
     console.log(process.env);
 
-    const {userInfo} = usePassageUserInfo();
+    const { userInfo, loading } = usePassageUserInfo();
 //    const { id } = userInfo;
     const [listings, setListings] = React.useState([]);
-console.log(userInfo);
+console.log("userInfo:", userInfo);
 console.log("listing:", listings);
+console.log("loading:", loading);
 
     // fetch listings from db
     React.useEffect(() => {
         const getAllListings = async () => {
-            const allListings = await axios.get(SERVER_URL+'/message/penpals/6530076dd128d58567d48136'); // userInfo.id)
+            const allListings = await axios.get(SERVER_URL+'/message/penpals/'+ userInfo.id); //6530076dd128d58567d48136'); // 
             console.log(allListings.data.writers)
             setListings(allListings.data.writers)
         }
