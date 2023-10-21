c76c3c173bf960e39db026841148447dc45f3f1c
Mary Rodriguez <maryrodri3210@gmail.com>
2023-10-20 22:13:06 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const messageController = require("./controllers/message-controller");
 
 const app = express();
 const PORT = 8000;
-const CLIENT_URL = "http://localhost:3001";
+const CLIENT_URL = "http://localhost:3000";
 
 require("dotenv").config();
 require("./config/db.connection");
