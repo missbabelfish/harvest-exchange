9fdbcb1d96b607b6356abea4c5d7900ba20f7490
Mary Rodriguez <maryrodri3210@gmail.com>
2023-10-22 19:10:06 -0500

    Reorganized routes

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -6,20 +6,23 @@ import Home from "./views/Home"
 import Login from "./views/Login";
 import Dashboard from "./views/Dashboard";
 import Profile from "./views/Profile";
-
-import styles from "./styles/App.module.css";
+import Listing from "./views/Listing";
+import EditListing from "./views/EditListing";
+import CreateListing from "./views/CreateListing";
 
 function App() {
   return (
     <PassageProvider appId={process.env.REACT_APP_PASSAGE_APP_ID}>
       <div>
           <Routes>
-          <Route path="/" element={<Home />}></Route>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/listing" element={<Listing />}></Route>
+            <Route path="/listing/CreateListing" element={<CreateListing/>}></Route>
+            <Route path="/listing/EditListing" element={<EditListing/>}></Route>
             <Route path="/login" element={<Login />}></Route>
             <Route path="/login/welcome" element={<Dashboard />}></Route>
             <Route path="/profile" element={<Profile />}></Route>
           </Routes>
-       
       </div>
     </PassageProvider>
   );
