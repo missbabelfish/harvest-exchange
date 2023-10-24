7c634db8f0c776878fa65978bbd6ff69d12336a3
Mary Rodriguez <maryrodri3210@gmail.com>
2023-10-23 20:20:53 -0500

    set value to filter values

--- frontend/src/components/ViewListings.js
+++ frontend/src/components/ViewListings.js
@@ -38,9 +38,9 @@ const PostList = () => {
       <h2>Listing posts</h2>
     <form >
         <label for="location">Location:</label>
-        <input type="text" id="location" name="location" value=""></input>
+        <input type="text" id="location" name="location" value={filter.location} onChange={handleChange}></input>
         <label for="distance">Distance:</label>
-        <input type="text" id="distance" name="distance" value=""></input>
+        <input type="text" id="distance" name="distance" value={filter.distance} onChange={handleChange}></input>
         <label for="category">category:</label>
         <select name="category" id="category">
             <option value="produce">produce</option>
@@ -48,7 +48,7 @@ const PostList = () => {
             <option value="live plant">live plant</option>
         </select>
         <label for="price">Price:</label>
-        <input type="number" id="price" name="price" value=""></input>
+        <input type="number" id="price" name="price" value={filter.price} onChange={handleChange}></input>
     </form>
 
       <ul>
