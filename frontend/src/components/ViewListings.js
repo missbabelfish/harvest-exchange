951ba2ceb421a305f0d09e50aa72ddf31ebc105b
Mary Rodriguez <maryrodri3210@gmail.com>
2023-10-27 17:08:39 -0500

    users can now filter listings in the frontend

--- frontend/src/components/ViewListings.js
+++ frontend/src/components/ViewListings.js
@@ -8,7 +8,7 @@ const PostList = () => {
     location: "",
     distance: "",
     category: "",
-    price: 0
+    price: ""
   })
 
   const getPosts = async () => {
@@ -30,36 +30,78 @@ const PostList = () => {
     const userInput = { ...filter };
     userInput[event.target.name] = event.target.value;
     setFilter(userInput);
+    // console.log(filter)
   };
 
   const handleChange2 = (event) => {
     const userInput = { ...filter };
-    userInput[event.target.name] = event.target.value;
+    userInput[event.target.name] = parseInt(event.target.value);
     setFilter(userInput);
+    // console.log(filter)
+    // console.log(event.target.value)
   };
+
+  async function doFilterTask(e) {
+//     console.log('a');
+//     e.preventDefault();
+//     const url = (
+//       'http://localhost:8000/listing?' +
+//       new URLSearchParams(filter).toString()
+//     );
+  
+//     const result = await fetch(url)
+//       .then(response => response.json());
+  
+//     console.log('Fetched from: ' + url);
+//     console.log(result);
+//   }
+
+  try {
+    e.preventDefault();
+  const response2 = await fetch('http://localhost:8000/listing?' +
+  new URLSearchParams(filter).toString());
+  const allPosts2 = await response2.json();
+  console.log(allPosts2)
+  setPosts(allPosts2);
+} catch (err) {
+  console.error(err);
+}
+
+useEffect(() => {
+getPosts();
+}, []);
+  }
+
+
+
 
   return (
     <>
       <h2>Listing posts</h2>
-    <form >
-        <label for="location">Location:</label>
+    <form onSubmit={doFilterTask}>
+        <label htmlFor="location">Location:</label>
         <input type="text" id="location" name="location" value={filter.location} onChange={handleChange}></input>
-        <label for="distance">Distance:</label>
+        <label htmlFor="distance">Distance:</label>
         <select name="distance" id="distance" value={filter.distance} onChange={handleChange2}>
+            <option value="">miles away</option>
             <option value="5">5 miles</option>
             <option value="10">10 miles</option>
-            <option value="15">15 miles</option>
+            <option value="15">15 miles</option> 
             <option value="20">20 miles</option>
             <option value="25">25 miles</option>
         </select>
-        <label for="category">category:</label>
+        <label htmlFor="category">category:</label>
         <select name="category" id="category" value={filter.category} onChange={handleChange}>
             <option value="produce">produce</option>
             <option value="seed">seed</option>
             <option value="live plant">live plant</option>
         </select>
-        <label for="price">Price:</label>
-        <input type="number" id="price" name="price" value={filter.price} onChange={handleChange}></input>
+        <label htmlFor="price">Price:</label>
+        <input type="Number" id="price" name="price" onChange={handleChange2}></input>
+        {/* <input type="range" name="price" min="0" max="100" defaultValue="0" onChange={handleChange2} className="slider" id="myRange"></input> */}
+        <div className="btn postbtn1">
+                <input className="postBtn" type="submit" value="Post" />
+              </div>
     </form>
 
       <ul>
@@ -87,4 +129,5 @@ const PostList = () => {
     </>
   );
 };
+
 export default PostList
